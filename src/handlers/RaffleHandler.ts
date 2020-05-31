eafc49676d7f97489067d75612e7d27925f92f71
nightrich <misirlioglisamet@gmail.com>
2020-05-31 19:01:50 +0300

    precision timers added

--- src/handlers/RaffleHandler.ts
+++ src/handlers/RaffleHandler.ts
@@ -22,13 +22,22 @@ export class RaffleHandler{
 
         for(let raffle = await cursor.next(); raffle !== null; raffle = await cursor.next()){
             const finishAt = new Date(raffle.finishAt)
-            if(Date.now() >= +finishAt){
-                await raffle.updateOne({ status: 'FINISHED' })
-                await this.pubsub.publish(Constants.SUBSCRIPTIONS.ON_RAFFLE_FINISHED, {
-                    onRaffleFinished: raffle
-                })
+            const remaining = +finishAt - Date.now()
+            if(remaining <= 60 * 1000){
+                this.setRaffleInterval(remaining, raffle)
+            }else if(Date.now() >= +finishAt){
+                this.setRaffleInterval(1000, raffle)
             }
         }
     }
 
+    private setRaffleInterval(timeout: number, raffle: any): void{
+        setTimeout(async () => {
+            await raffle.updateOne({ status: 'FINISHED' })
+            await this.pubsub.publish(Constants.SUBSCRIPTIONS.ON_RAFFLE_FINISHED, {
+                onRaffleFinished: raffle
+            })
+        }, timeout)
+    }
+    
 }
