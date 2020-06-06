28872425d0b05a3eb7b358f1094a60ff122aa5ea
nightrich <misirlioglisamet@gmail.com>
2020-06-06 03:40:43 +0300

    model update to typescript

--- src/models/Raffle.ts
+++ src/models/Raffle.ts
@@ -1,7 +1,17 @@
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
+import mongoose, { Schema, Document } from 'mongoose';
 
-const RaffleSchema = new Schema({
+export interface Raffle extends Document{
+    prize: string
+    server_id: string
+    constituent_id: string
+    channel_id: string
+    message_id: string
+    numbersOfWinner: number
+    status: string
+    finishAt: Date
+}
+
+const RaffleSchema: Schema = new Schema({
     prize: {
         type: String,
         required: true
@@ -38,4 +48,4 @@ const RaffleSchema = new Schema({
     timestamps: true
 });
 
-module.exports = mongoose.model('Raffle', RaffleSchema);
+export default mongoose.model<Raffle>('Raffle', RaffleSchema)
