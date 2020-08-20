ade027541c1e923382155c0c45eb39ae564c0107
nightrich <misirlioglisamet@gmail.com>
2020-08-20 14:31:33 +0300

    Hmm

--- src/tasks/predefined/RaffleTask.ts
+++ src/tasks/predefined/RaffleTask.ts
@@ -22,57 +22,57 @@ export default class RaffleTask extends Task<Raffle>{
     }
 
     protected setInterval(timeout: number, raffle: Raffle){
-            new Promise((resolve, reject) => {
-                this.client
-                    .fetchMessage(raffle.server_id, raffle.channel_id, raffle.message_id)
-                    .then(async result => {
-                        const updateCallback = async (
-                            customTime: number,
-                            message: string = Raffle.getAlertMessage(),
-                            alert: boolean = true
-                        ) => {
-                            await result.edit(message, {
-                                embed: raffle.getEmbed(alert, customTime)
-                            }).catch(async (err: DiscordAPIError | HTTPError) => {
-                                const guild = result.guild
-                                if(guild){
-                                    const message = [
-                                        `<:down:744193243805384704> Sunucunuzda bulunan çekiliş bir **hatadan** dolayı sonuçlandırılamadı.`,
-                                        `Lütfen çekilişinizi kurtarabilmemiz için bu mesajın **ekran görüntüsü** ile birlikte bizimle iletişime geçin.\n`,
-                                        `> **Destek-İletişim:** https://discord.gg/CRgXhfs\n`,
-                                    ].join('\n')
+        new Promise((resolve, reject) => {
+            this.client
+                .fetchMessage(raffle.server_id, raffle.channel_id, raffle.message_id)
+                .then(async result => {
+                    const updateCallback = async (
+                        customTime: number,
+                        message: string = Raffle.getAlertMessage(),
+                        alert: boolean = true
+                    ) => {
+                        await result.edit(message, {
+                            embed: raffle.getEmbed(alert, customTime)
+                        }).catch(async (err: DiscordAPIError | HTTPError) => {
+                            const guild = result.guild
+                            if(guild){
+                                const message = [
+                                    `<:down:744193243805384704> Sunucunuzda bulunan çekiliş bir **hatadan** dolayı sonuçlandırılamadı.`,
+                                    `Lütfen çekilişinizi kurtarabilmemiz için bu mesajın **ekran görüntüsü** ile birlikte bizimle iletişime geçin.\n`,
+                                    `> **Destek-İletişim:** https://discord.gg/CRgXhfs\n`,
+                                ].join('\n')
 
-                                    try{
-                                        await guild.owner?.send(message, {
-                                            embed: this.client.buildErrorReporterEmbed(guild, err)
-                                        })
-                                    }catch(err){
-                                        return reject(err)
-                                    }
+                                try{
+                                    await guild.owner?.send(message, {
+                                        embed: this.client.buildErrorReporterEmbed(guild, err)
+                                    })
+                                }catch(err){
+                                    return reject(err)
                                 }
+                            }
 
-                                return reject(err)
-                            })
-                        }
+                            return reject(err)
+                        })
+                    }
 
-                        setTimeout(async () => {
-                            await updateCallback(10, Raffle.getStartMessage(), false)
-                            await this.sleep(7 * 1000)
-                            await updateCallback(3)
-                            await this.sleep(1000)
-                            await updateCallback(2)
-                            await this.sleep(1000)
-                            await updateCallback(1)
+                    setTimeout(async () => {
+                        await updateCallback(10, Raffle.getStartMessage(), false)
+                        await this.sleep(7 * 1000)
+                        await updateCallback(3)
+                        await this.sleep(1000)
+                        await updateCallback(2)
+                        await this.sleep(1000)
+                        await updateCallback(1)
 
-                            resolve()
-                        }, timeout - (10 * 1000))
-                    })
-                    .catch(reject)
-            }).catch(async () => {
-                await raffle.setStatus('FINISHED')
-            }).then(() => {
-                super.setInterval(500, raffle)
-            })
+                        resolve()
+                    }, timeout - (10 * 1000))
+                })
+                .catch(reject)
+        }).catch(async () => {
+            await raffle.setStatus('FINISHED')
+        }).then(() => {
+            super.setInterval(500, raffle)
+        })
     }
 
     protected intervalCallback(raffle: Raffle): () => void{
