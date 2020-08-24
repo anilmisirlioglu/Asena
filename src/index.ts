4a7b0145337492cea6e62ec62c8397078a47cc72
nightrich <misirlioglisamet@gmail.com>
2020-08-25 02:12:40 +0300

    prototype functions added

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
+/// <reference path='../typings/index.d.ts'/>
+
 import dotenv from 'dotenv';
 import Asena from './Asena';
 import MongoDB from './drivers/MongoDB';
@@ -17,3 +19,11 @@ client.login(process.env[client.version.isDev() ? 'TOKEN_DEV' : 'TOKEN'])
 process.on('unhandledRejection', (rej, promise) => {
     client.logger.error(rej?.toString() ?? 'Unknown Error')
 })
+
+String.prototype.removeWhiteSpaces = function(){
+    return this.replace(/\s|\x00|\x0B/g,'')
+}
+
+Array.prototype.checkIfDuplicateExists = function(){
+    return new Set(this).size !== this.length
+}
