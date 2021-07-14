57636f75ab1e6aece3b14b28b61ee29ed4426bb7
Anıl Mısırlıoğlu <misirlioglisamet@gmail.com>
2021-07-14 18:40:42 +0300

    refactor: added more emojis on soundaway and raffle embeds
    Signed-off-by: Anıl Mısırlıoğlu <misirlioglisamet@gmail.com>

--- src/structures/Raffle.ts
+++ src/structures/Raffle.ts
@@ -7,20 +7,20 @@ import {
     TextChannel
 } from 'discord.js';
 import Structure from './Structure';
-import RaffleModel, { IRaffle, RaffleStatus } from '../models/Raffle';
+import RaffleModel, {IRaffle, RaffleStatus} from '../models/Raffle';
 import Timestamps from '../models/legacy/Timestamps';
-import { secondsToString } from '../utils/DateTimeHelper';
-import Constants, { URLMap } from '../Constants';
+import {secondsToString} from '../utils/DateTimeHelper';
+import Constants, {URLMap} from '../Constants';
 import RandomArray from '../utils/RandomArray';
 import ID from '../models/legacy/ID';
 import SuperClient from '../SuperClient';
 import Server from './Server';
-import { parseGiveawayTimerURL } from "../utils/Utils";
+import {parseGiveawayTimerURL} from "../utils/Utils";
 
 type SuperRaffle = IRaffle & Timestamps & ID
 
 // TODO::keep server model in cache
-class Raffle extends Structure<typeof RaffleModel, SuperRaffle>{
+class Raffle extends Structure<typeof RaffleModel, SuperRaffle> {
 
     public prize: string
     public server_id: Snowflake
@@ -31,11 +31,11 @@ class Raffle extends Structure<typeof RaffleModel, SuperRaffle>{
     public status: RaffleStatus
     public finishAt: Date
 
-    constructor(data: SuperRaffle){
+    constructor(data: SuperRaffle) {
         super(RaffleModel, data)
     }
 
-    protected patch(data: SuperRaffle){
+    protected patch(data: SuperRaffle) {
         this.prize = data.prize
         this.server_id = data.server_id
         this.constituent_id = data.constituent_id
@@ -47,39 +47,39 @@ class Raffle extends Structure<typeof RaffleModel, SuperRaffle>{
         this.finishAt = data.finishAt
     }
 
-    protected identifierKey(): string{
+    protected identifierKey(): string {
         return 'message_id'
     }
 
-    public isContinues(): boolean{
+    public isContinues(): boolean {
         return this.status === 'CONTINUES'
     }
 
-    public async setStatus(status: RaffleStatus){
-        await this.update({ status })
+    public async setStatus(status: RaffleStatus) {
+        await this.update({status})
     }
 
-    public isCancelable(): boolean{
+    public isCancelable(): boolean {
         return this.isContinues()
     }
 
-    public async setCanceled(){
+    public async setCanceled() {
         await this.setStatus('CANCELED')
     }
 
-    public async finish(client: SuperClient, server: Server){
+    public async finish(client: SuperClient, server: Server) {
         await this.setStatus('FINISHED')
 
         const channel: GuildChannel | undefined = await client.fetchChannel(this.server_id, this.channel_id)
-        if(channel instanceof TextChannel){
+        if (channel instanceof TextChannel) {
             const message: Message | undefined = await channel.messages.fetch(this.message_id)
-            if(message instanceof Message){
+            if (message instanceof Message) {
                 const winners: string[] = await this.identifyWinners(message)
                 const _message: string = this.getMessageURL()
                 const winnersOfMentions: string[] = winners.map(winner => `<@${winner}>`)
 
                 let description, content
-                switch(winners.length){
+                switch (winners.length) {
                     case 0:
                         description = server.translate('structures.raffle.winners.none.description')
                         content = server.translate('structures.raffle.winners.none.content')
@@ -91,14 +91,17 @@ class Raffle extends Structure<typeof RaffleModel, SuperRaffle>{
                         break
 
                     default:
-                        description = `${server.translate('structures.raffle.winners.plural.description')}:\n${winnersOfMentions.join('\n')}`
+                        description = `${server.translate('structures.raffle.winners.plural.description')}:\n${winnersOfMentions.map(winner => `:small_blue_diamond: ${winner}`).join('\n')}`
                         content = server.translate('structures.raffle.winners.plural.content', winnersOfMentions.join(', '), this.prize)
                         break
                 }
 
                 const embed: MessageEmbed = new MessageEmbed()
                     .setAuthor(this.prize)
-                    .setDescription(`:medal: ${description}\n:reminder_ribbon: ${server.translate('structures.raffle.creator')}: <@${this.constituent_id}>`)
+                    .setDescription([
+                        `:medal: ${description}`,
+                        `:reminder_ribbon: ${server.translate('structures.raffle.creator')}: <@${this.constituent_id}>`
+                    ])
                     .setFooter(`${server.translate('structures.raffle.footer.text', this.numbersOfWinner)} | ${server.translate('structures.raffle.footer.finish')}`)
                     .setTimestamp(new Date(this.finishAt))
                     .setColor('#36393F')
@@ -116,18 +119,18 @@ class Raffle extends Structure<typeof RaffleModel, SuperRaffle>{
     public async identifyWinners(
         message: Message,
         numberOfWinners: number = this.numbersOfWinner
-    ): Promise<string[]>{
+    ): Promise<string[]> {
         const winners = []
-        if(message){
+        if (message) {
             const reaction: MessageReaction | undefined = await message.reactions.cache.get(Constants.CONFETTI_REACTION_EMOJI)
             const [_, users] = (await reaction.users.fetch()).partition(user => user.bot)
             const userKeys = users.keyArray().filter(user_id => user_id !== this.constituent_id)
 
-            if(userKeys.length > numberOfWinners){
+            if (userKeys.length > numberOfWinners) {
                 const random = new RandomArray(userKeys)
                 random.shuffle()
                 winners.push(...random.random(numberOfWinners))
-            }else{
+            } else {
                 winners.push(...userKeys)
             }
         }
@@ -135,19 +138,19 @@ class Raffle extends Structure<typeof RaffleModel, SuperRaffle>{
         return winners
     }
 
-    public getMessageURL(): string{
+    public getMessageURL(): string {
         return `https://discordapp.com/channels/${this.server_id}/${this.channel_id}/${this.message_id}`
     }
 
-    public static getStartMessage(server: Server): string{
+    public static getStartMessage(server: Server): string {
         return `${Constants.CONFETTI_EMOJI} **${server.translate('structures.raffle.messages.start')}** ${Constants.CONFETTI_EMOJI}`
     }
 
-    public static getAlertMessage(server: Server): string{
+    public static getAlertMessage(server: Server): string {
         return `${Constants.CONFETTI_EMOJI} **${server.translate('structures.raffle.messages.alert')}** ${Constants.CONFETTI_EMOJI}`
     }
 
-    public getEmbed(server: Server, alert: boolean = false, customRemainingTime: number = undefined): MessageEmbed{
+    public getEmbed(server: Server, alert: boolean = false, customRemainingTime: number = undefined): MessageEmbed {
         const length = Math.ceil((+this.finishAt - +this.createdAt) / 1000)
         const time = secondsToString(length, server.locale)
         const remaining = secondsToString(customRemainingTime ?? Math.ceil((+this.finishAt - Date.now()) / 1000), server.locale)
