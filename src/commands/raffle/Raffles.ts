357795a868b1e7db9c1cfd3e6c53ce591bc2d718
Anıl Mısırlıoğlu <misirlioglisamet@gmail.com>
2021-07-14 15:41:26 +0300

    chore: raffles command aliases changed
    Signed-off-by: Anıl Mısırlıoğlu <misirlioglisamet@gmail.com>

--- src/commands/raffle/Raffles.ts
+++ src/commands/raffle/Raffles.ts
@@ -11,7 +11,7 @@ export default class Raffles extends Command{
     constructor(){
         super({
             name: 'raffles',
-            aliases: ['giveaway', 'giveaways', 'çekilişler', 'aktifçekilişler', 'cekilisler', 'activeraffles', 'list'],
+            aliases: ['giveaway', 'giveaways', 'çekilişler', 'cekilisler', 'list'],
             description: 'commands.raffle.list.description',
             usage: null,
             permission: undefined,
