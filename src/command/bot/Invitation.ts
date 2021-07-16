3dfa3ec1d38a31d9512512e8b925ca0ba6bbe8a7
Anıl Mısırlıoğlu <misirlioglisamet@gmail.com>
2021-07-16 11:56:23 +0300

    refactor: `davet` command name changed to `invite`
    Signed-off-by: Anıl Mısırlıoğlu <misirlioglisamet@gmail.com>

--- src/command/bot/Invitation.ts
+++ src/command/bot/Invitation.ts
@@ -8,8 +8,8 @@ export default class Invitation extends Command{
 
     constructor(){
         super({
-            name: 'davet',
-            aliases: ['invite', 'party', 'davetiye', 'link', 'wiki'],
+            name: 'invite',
+            aliases: ['davet', 'party', 'davetiye', 'link', 'wiki'],
             description: 'commands.bot.invitation.description',
             usage: null,
             permission: undefined,
