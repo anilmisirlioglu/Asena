ca33485d894d49fc45fefd47f413ced39ed388d4
Anıl Mısırlıoğlu <misirlioglisamet@gmail.com>
2021-08-17 21:39:04 +0300

    refactor(logger): logs will no longer be written to a file
    Signed-off-by: Anıl Mısırlıoğlu <misirlioglisamet@gmail.com>

--- src/utils/Logger.ts
+++ src/utils/Logger.ts
@@ -1,8 +1,7 @@
-import fs from 'fs';
 import TextFormat, { Colors } from './TextFormat';
 
 enum LogLevel{
-    ERROR = `ERROR`,
+    ERROR = 'ERROR',
     WARNING = 'WARN',
     INFO = 'INFO',
     DEBUG = 'DEBUG'
@@ -16,19 +15,12 @@ enum Space{
 
 export default class Logger{
 
-    private static LOG = 'log.txt';
-    private static LOG_DIR = `${__dirname}/../../` + Logger.LOG;
-
     private readonly worker: string
 
     constructor(worker: string){
         this.worker = worker
 
         TextFormat.init()
-
-        if(!fs.existsSync(Logger.LOG)){
-            fs.closeSync(fs.openSync(Logger.LOG, 'w+'))
-        }
     }
 
     private send(message: string, level: ColorizedLogLevel): void{
@@ -38,12 +30,6 @@ export default class Logger{
             .replace(/\..+/, '')
 
         const format = `${Colors.DARK_GRAY}${date} ${level} ${Colors.DARK_PURPLE}${process.pid} ${Colors.WHITE}[${Colors.AQUA}${this.space(this.worker)}${Colors.WHITE}] ${Colors.GOLD}> ${Colors.WHITE}${message}`;
-
-        fs.appendFileSync(
-            Logger.LOG_DIR,
-            `\n${TextFormat.clean(format)}`
-        );
-
         console.log(TextFormat.toANSI(format + Colors.RESET));
     }
 
@@ -51,20 +37,9 @@ export default class Logger{
         return text.length >= space ? text.slice(0, space) : ' '.repeat(space - text.length) + text
     }
 
-    public error(message: string): void{
-        this.send(message, Colors.RED + LogLevel.ERROR);
-    }
-
-    public warning(message: string): void{
-        this.send(message, Colors.GOLD + LogLevel.WARNING + ' ');
-    }
-
-    public info(message: string): void{
-        this.send(message, Colors.GREEN + LogLevel.INFO + ' ');
-    }
-
-    public debug(message: string): void{
-        this.send(message, Colors.AQUA + LogLevel.DEBUG);
-    }
+    error   = (message: string) => this.send(message, Colors.RED + LogLevel.ERROR)
+    warning = (message: string) => this.send(message, Colors.GOLD + LogLevel.WARNING + ' ')
+    info    = (message: string) => this.send(message, Colors.GREEN + LogLevel.INFO + ' ')
+    debug   = (message: string) => this.send(message, Colors.AQUA + LogLevel.DEBUG)
 
 }
