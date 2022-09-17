dac280a344e516531762e6dde4d5683442bf21d6
Anıl Mısırlıoğlu <misirlioglisamet@gmail.com>
2022-09-17 21:11:09 +0300

    chore: lint

--- cmd/bot/Help.ts
+++ cmd/bot/Help.ts
@@ -5,13 +5,12 @@ export default class Help extends ApplicationCommand{
     build(){
         this.setName('help')
         this.setDescription('Responds with a list of commands you can use, and information about Asena')
-        this.setDescriptionLocalizations({ tr: 'Komutlar hakkında bilgi almak için yardım menüsünü gönderir' })
-        this.addStringOption(option =>
-            option
-                .setName('command')
-                .setNameLocalizations({ tr: 'komut' })
-                .setDescription('The sub command you want to see the help menu')
-                .setDescriptionLocalizations({ tr: 'Detaylı yardım almak istediğiniz komut' })
+        this.setDescriptionLocalizations({tr: 'Komutlar hakkında bilgi almak için yardım menüsünü gönderir'})
+        this.addStringOption(option => option
+            .setName('command')
+            .setNameLocalizations({tr: 'komut'})
+            .setDescription('The sub command you want to see the help menu')
+            .setDescriptionLocalizations({tr: 'Detaylı yardım almak istediğiniz komut'})
         )
     }
 
